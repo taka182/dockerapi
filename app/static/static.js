7de5dacdff731f1deaddb67e03acb4c726b4db6d
Taka <soutakaoka92@gmail.com>
2023-08-21 11:24:11 +0900

    ソースコードのリファクタリング。 UIの状態を切り替えるtoggleUIState 投稿を更新するupdatePost 編集状態によって処理を変更するtoggleEditMode関数を作成

--- app/static/static.js
+++ app/static/static.js
@@ -15,20 +15,30 @@ function deletePost(post_id) {
         });
 }
 
-// 編集ボタン押下時、編集可能状態にする。保存ボタン押下時、保存する。
+// 編集ボタン押下時、編集可能状態にする。保存ボタン押下時、保存する
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.editButton, .saveButton').forEach(button => {
+    document.querySelectorAll('editButton, saveButton').forEach(button => {
         button.addEventListener('click', function () {
             toggleEditMode(this.getAttribute('post_id'), this.classList.contains('editButton'));
         });
     });
 });
 
+// 編集状態のときはreturn,編集状態ではない場合は投稿を更新
 function toggleEditMode(postId, isEditMode) {
+    toggleUIState(postId, isEditMode);
+    if (isEditMode) {
+        return
+    }
+    updatePost(postId);
+}
+
+// UIの状態を切り替える
+function toggleUIState(postId, isEditMode) {
     const titleElement = document.getElementById(`title-${postId}`);
     const descElement = document.getElementById(`desc-${postId}`);
-    const editButton = document.querySelector(`.editButton[post_id="${postId}"]`);
-    const saveButton = document.querySelector(`.saveButton[post_id="${postId}"]`);
+    const editButton = document.querySelector(`editButton[post_id="${postId}"]`);
+    const saveButton = document.querySelector(`saveButton[post_id="${postId}"]`);
 
     titleElement.contentEditable = isEditMode;
     descElement.contentEditable = isEditMode;
@@ -41,41 +51,43 @@ function toggleEditMode(postId, isEditMode) {
         titleElement.focus();
         return;
     }
+}
 
+// 投稿を更新する
+function updatePost(postId) {
+    const titleElement = document.getElementById(`title-${postId}`);
+    const descElement = document.getElementById(`desc-${postId}`);
     const titleText = titleElement.textContent.trim();
     const descText = descElement.textContent.trim();
 
+    // 文字が入力されているかチェック
     if (titleText === "" || descText === "") {
         alert("タイトルまたは投稿内容が空白です。");
-        titleElement.contentEditable = true;
-        descElement.contentEditable = true;
-        // 編集ボタンの非表示
-        editButton.style.display = 'none';
-        // 保存ボタンの表示
-        saveButton.style.display = 'inline-block';
-        titleElement.focus();
-    } else {
-        // サーバに編集内容を送信する
-        const updatedPost = {
-            title: titleText,
-            description: descText
-        };
-        fetch(`/posts/${postId}/`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(updatedPost)
-        })
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === 'success') {
-                    alert('投稿内容を更新しました!');
-                    location.reload();
-                } else {
-                    alert('更新に失敗しました');
-                    location.reload();
-                }
-            });
+        toggleUIState(postId, true);
+        return;
     }
+
+    const updatedPost = {
+        title: titleText,
+        description: descText
+    };
+
+    // サーバに編集内容を送信する
+    fetch(`/posts/${postId}/`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedPost)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'success') {
+                alert('投稿内容を更新しました!');
+                location.reload();
+            } else {
+                alert('更新に失敗しました');
+                location.reload();
+            }
+        });
 }
